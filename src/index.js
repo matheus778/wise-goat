032dd40eab58e80f0f36c3ef7ba0fd95d647ef73
matheus778 <matheuzv777@gmail.com>
2021-01-02 12:10:33 -0300

    adicionada a fonte roboto

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import HomePage from './pages/HomePage';
 import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,400&display=swap');
 body {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Roboto', sans-serif
 }
 `;
 
